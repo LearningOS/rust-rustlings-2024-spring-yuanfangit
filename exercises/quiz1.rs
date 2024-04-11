0e50568b8d943d3d2f9d1fd4dbfd3a5934f97c4b
yuanfangit <130342264+yuanfangit@users.noreply.github.com>
2024-04-11 16:05:47 +0000

    30道提交

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(a:i32) ->i32{
+    if a > 40 {
+        a
+    } else {
+        2 * a
+    }
+}
 
 // Don't modify this function!
 #[test]
@@ -31,3 +37,4 @@ fn verify_test() {
     assert_eq!(41, price3);
     assert_eq!(65, price4);
 }
+
